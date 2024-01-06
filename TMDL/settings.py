16ba292ab15f299593dac95872989fcd0da63800
Tayeb EL OUADRASSI TLIDI <t.elouadrassi@gmail.com>
2024-01-06 17:34:40 +0100

    Minor modifications

--- TMDL/settings.py
+++ TMDL/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -93,6 +94,9 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DB_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
